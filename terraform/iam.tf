40e1524b7d5cba479005d4176e923b0bd0733372
Pratikkumar Patel <140781526+Pratikkumar2687@users.noreply.github.com>
2026-07-22 15:30:07 -0400

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,1 +1,94 @@
+# IAM Role for Lambda
+# Implements least privilege security model
 
+
+resource "aws_iam_role" "lambda_execution_role" {
+
+  name = "${var.project_name}-lambda-role"
+
+
+  assume_role_policy = jsonencode({
+
+    Version = "2012-10-17"
+
+    Statement = [
+
+      {
+
+        Effect = "Allow"
+
+        Principal = {
+
+          Service = "lambda.amazonaws.com"
+
+        }
+
+        Action = "sts:AssumeRole"
+
+      }
+
+    ]
+
+  })
+
+}
+
+
+
+# Basic Lambda Logging Permission
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+
+  role = aws_iam_role.lambda_execution_role.name
+
+  policy_arn =
+    "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+
+}
+
+
+
+# Bedrock Access Policy
+
+resource "aws_iam_policy" "bedrock_access" {
+
+  name = "${var.project_name}-bedrock-policy"
+
+
+  policy = jsonencode({
+
+    Version = "2012-10-17"
+
+    Statement = [
+
+      {
+
+        Effect = "Allow"
+
+        Action = [
+
+          "bedrock:InvokeModel",
+
+          "bedrock:InvokeAgent"
+
+        ]
+
+        Resource = "*"
+
+      }
+
+    ]
+
+  })
+
+}
+
+
+
+resource "aws_iam_role_policy_attachment" "bedrock_policy_attachment" {
+
+  role = aws_iam_role.lambda_execution_role.name
+
+  policy_arn = aws_iam_policy.bedrock_access.arn
+
+}
